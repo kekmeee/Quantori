45119f8e2206696bab4415bb4410053c384443be
Reshetnyak Danil <stdanil931361@gmail.com>
2021-08-14 18:20:00 +0300

    edit hw10

--- hw10.py
+++ hw10.py
@@ -1,10 +1,17 @@
-number = int(input("Enter number: "))
-count = int()
-while number != 1:
-    if number % 2 == 0 and number != 1:
-        number //= 2
-        count += 1
-    else:
-        number = number * 3 + 1
-        count += 1
-print(count)
+def countKollatza(number=int()):
+    count = int()
+    while number != 1:
+        if number % 2 == 0 and number != 1:
+            number //= 2
+            count += 1
+        else:
+            number = number * 3 + 1
+            count += 1
+    return count
+
+print(f"Count for 11: {countKollatza(11)}")
+print(f"Count for 12: {countKollatza(12)}")
+print(f"Count for 13: {countKollatza(13)}")
+print(f"Count for 27: {countKollatza(27)}")
+print(f"Count for 54: {countKollatza(54)}")
+print(f"Count for 108: {countKollatza(108)}")
