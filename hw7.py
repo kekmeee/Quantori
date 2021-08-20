e47f351bf40837543806eeaba51190369ce24a7f
unknown <C:\Users\res_da\AppData\Roaming\The Bat!>
2021-08-20 15:58:35 +0300

    edit hw7: change dir for files

--- hw7.py
+++ hw7.py
@@ -2,7 +2,7 @@ dic_users = dict()  # словарь username:uid
 dic_shells = dict()  # словарь  shell:count
 dic_groups_uid = dict()  # словарь  groupname: [uid]
 #  Читаем файл etc/passwd и записываем в словарь dic_users и dic_shells
-passwd = open("etc/passwd", "r")
+passwd = open("tmp/etc/passwd", "r")
 for p_str in passwd:
     dic_users[p_str.split(":")[0]] = p_str.split(":")[2]
     shell = p_str.split(":")[-1][:-1]
@@ -14,7 +14,7 @@ passwd.close()
 # Читаем файл etc/group
 # Находим имя группы и пользователей, которые в нее входят
 # Для каждого имени группы берем имя пользователя и из словаря dic_users берем uid и записываем в словарь dic_groups_uid
-group = open("etc/group", "r")
+group = open("tmp/etc/group", "r")
 for g_str in group:
     group_usernames = g_str.split(":")[3][:-1].split(",")
     if group_usernames != [""]:  # Проверка наличия пользователей у группы
@@ -31,5 +31,5 @@ output = open("output.txt", "w")
 for sh, count in dic_shells.items():
     output.write(f"{sh} - {count};\n")
 for g_name, u in dic_groups_uid.items():
-    uid = ''.join((f"{x}," for x in u))
+    uid = ','.join((f"{x}" for x in u))
     output.write(f"{g_name}:{uid}\n")
